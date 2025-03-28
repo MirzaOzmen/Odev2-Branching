08a410906c15b2dbb019f53df79f7fee8c309064
Mirza <mirza.ozmen@ogr.deu.edu.tr>
2025-03-28 15:51:21 +0300

    Character Color Change Script Updated

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,18 +6,27 @@ public class Movement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private SpriteRenderer sp;
     private void Start()
     {
-       rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
+        sp = GetComponent<SpriteRenderer>();
     }
     void Update()
     {
-        
 
         float vertical = Input.GetAxisRaw("Vertical");
         float horizontal = Input.GetAxisRaw("Horizontal");
-        Vector2 BasicMovement = new Vector2(horizontal, vertical) *moveSpeed * Time.deltaTime;
-        transform.Translate(BasicMovement);        
+        Vector2 BasicMovement = new Vector2(horizontal, vertical) * moveSpeed * Time.deltaTime;
+        transform.Translate(BasicMovement);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            sp.color = Color.red;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            sp.color = Color.green;
+        }
     }
 
 }
