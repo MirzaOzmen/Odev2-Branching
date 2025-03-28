54370a597ba954547c069ec2ad3d98661bbeab0e
Mirza <mirza.ozmen@ogr.deu.edu.tr>
2025-03-28 15:27:39 +0300

    Karakter Movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    [SerializeField] private Rigidbody2D rb;
+    private void Start()
+    {
+       rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        
+
+        float vertical = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        Vector2 BasicMovement = new Vector2(horizontal, vertical) *moveSpeed * Time.deltaTime;
+        transform.Translate(BasicMovement);        
+    }
+
+}
