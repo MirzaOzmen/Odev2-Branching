6b5579f81c0a99cd65f3024c6ed9a111f5ff63cf
Mirza <mirza.ozmen@ogr.deu.edu.tr>
2025-03-28 15:44:24 +0300

    Character color changing mechanic added and character prefab Created

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,9 +6,11 @@ public class Movement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private SpriteRenderer sp;
     private void Start()
     {
        rb = GetComponent<Rigidbody2D>();
+        sp = GetComponent<SpriteRenderer>();
     }
     void Update()
     {
@@ -17,7 +19,15 @@ public class Movement : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         float horizontal = Input.GetAxisRaw("Horizontal");
         Vector2 BasicMovement = new Vector2(horizontal, vertical) *moveSpeed * Time.deltaTime;
-        transform.Translate(BasicMovement);        
+        transform.Translate(BasicMovement); 
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            sp.color = Color.red;
+        }
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            sp.color = Color.black;
+        }
     }
 
 }
